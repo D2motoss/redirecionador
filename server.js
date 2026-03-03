250ba93bb471f36afebfafc58fd7ad43cbc30f18
Lucas Eccel <lucaseccel19@gmail.com>
2026-03-03 09:53:18 -0300

    estilizado

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
-const express = require("express");
-const app = express();
+document.addEventListener("DOMContentLoaded", () => {
+  const botao = document.getElementById("btnWhatsapp");
 
-// Lista dos WhatsApps
-const whatsappLinks = [
-  "https://wa.me/5547996264683",
-  "https://wa.me/5547996834199"
-];
+  if (!botao) {
+    console.error("Botão WhatsApp não encontrado");
+    return;
+  }
 
-// Controle simples para alternar
-let contador = 0;
+  botao.addEventListener("click", () => {
+    const links = [
+       "https://wa.me/5547996264683",
+       "https://wa.me/5547996834199"
+    ];
 
-// Rota principal (LINK DA BIO)
-app.get("/", (req, res) => {
-  const linkEscolhido = whatsappLinks[contador];
-  contador = (contador + 1) % whatsappLinks.length;
-
-  console.log("Redirecionando para:", linkEscolhido);
-  res.redirect(linkEscolhido);
-});
-
-// Porta dinâmica (Render) ou local
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Servidor WhatsApp Redirect ativo 🚀");
+    const escolhido = Math.floor(Math.random() * links.length);
+    window.location.href = links[escolhido];
+  });
 });
